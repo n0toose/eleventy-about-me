e18cb5d302ffc22545636dc239fe1d035354188b
Panos V <hello@alwayslivid.com>
2019-04-24 18:41:51 +0300

    Bug fixes. Removed leftover code.

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
     eleventyConfig.addPassthroughCopy('css')
     eleventyConfig.addPassthroughCopy('assets')
     return {
