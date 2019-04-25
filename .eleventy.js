fc257640450c603746a0483e6a02a7631a891e76
Panos V <hello@alwayslivid.com>
2019-04-25 14:28:37 +0300

    Added GitHub pages preview.

--- .eleventy.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
-    eleventyConfig.addPassthroughCopy('./src/css')
-    eleventyConfig.addPassthroughCopy('./src/assets')
-    return {
-      passthroughFileCopy: true
-    }
-  }
